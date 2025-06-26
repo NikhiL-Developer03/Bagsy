8c02ab80199ec5089cd08846d26bbae13acfa3df
Nikhil.Developer03 <nikhil.developer03@gmail.com>
2025-06-26 13:03:39 +0530

    feat: add mongoose models and clean up project structure

--- notes.js
+++ notes.js
@@ -1,19 +1,42 @@
-const express = require("express")
-const app = express();
+// const express = require("express")
+// const app = express();
+// const { name } = require("ejs")
 
-const cookieParser = require("cookie-parser")
-const path = require("path")
-const port = 3000
+// const cookieParser = require("cookie-parser")
+// const path = require("path")
+// const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("hey");
-})
+// app.get("/", (req, res) => {
+//   res.send("hey");
+// })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`http://localhost:${port}`)
+// })
+
+// Imp Db connection string ek bar hi use hoti hai 
+
+// userModel ->
+// full name - string
+// email - string
+// password - string
+// cart - array
+// isadmin - boolean
+// order - array
+// contact - number
+// picture - db
+
+// productModel
+// image
+// name
+// price
+// discount
+// bgcolor
+// panelcolor
+// textcolor
+
