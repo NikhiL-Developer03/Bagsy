e8a197ace2acaa9788c01ed93868f167469670fa
Nikhil Singh <nikhil.developer03@gmail.com>
2025-06-30 22:27:38 +0530

    feat: add conditional navigation menu based on user login status

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,16 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
-  res.render("index", { error });
+  res.render("index", { error, loggedin: false });
 });
 
 router.get("/shop", isLoggedIn, async (req, res) => {
   let products = await productModel.find();
-  res.render("shop", { products});
+  res.render("shop", { products });
+});
+
+router.get("/logout", isLoggedIn, (req, res) => {
+  res.render("shop");
 });
 
 module.exports = router;
