d6b8e169f98eba9758f2e18436d53aee70000bed
Nikhil Singh <nikhil.developer03@gmail.com>
2025-07-01 15:35:24 +0530

    feat: implement shopping cart functionality with add to cart and cart display

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -10,7 +11,22 @@ router.get("/", (req, res) => {
 
 router.get("/shop", isLoggedIn, async (req, res) => {
   let products = await productModel.find();
-  res.render("shop", { products });
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+router.get("/cart", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email }).populate("cart");
+  const bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount)
+  res.render("cart", { user, bill });
+});
+
+router.get("/addtocart/:productid", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email })
+  console.log(user);
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Added to cart")
+  res.redirect("/shop")
 });
 
 router.get("/logout", isLoggedIn, (req, res) => {
